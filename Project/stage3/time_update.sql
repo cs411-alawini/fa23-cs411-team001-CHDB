5b396e2848c4715ad947365f1c129d202fb54b0a
yifeis7 <yifeis7@illinois.edu>
2023-10-29 15:16:48 -0500

    fix bug

--- Project/stage3/time_update.sql
+++ Project/stage3/time_update.sql
@@ -3,23 +3,23 @@ SET SQL_SAFE_UPDATES = 0;
 UPDATE Time
 SET 
     Time_MostViewed_VideoId = (
-        SELECT v.Video_id
+        SELECT vi.Video_Id
         FROM View AS vi 
-        WHERE v.Trending_Date = Time.Trending_Date
+        WHERE vi.Trending_Date = Time.Trending_Date
         ORDER BY vi.Views DESC
         LIMIT 1
     ),
     Time_MostLike_VideoId = (
-        SELECT v.Video_id
+        SELECT vi.Video_Id
         FROM View AS vi 
-        WHERE v.Trending_Date = Time.Trending_Date
+        WHERE vi.Trending_Date = Time.Trending_Date
         ORDER BY vi.Likes DESC
         LIMIT 1
     ),
     Time_MostDislike_VideoId = (
-        SELECT v.Video_id
+        SELECT vi.Video_Id
         FROM View AS vi 
-        WHERE v.Trending_Date = Time.Trending_Date
+        WHERE vi.Trending_Date = Time.Trending_Date
         ORDER BY vi.Dislikes DESC
         LIMIT 1
     );
